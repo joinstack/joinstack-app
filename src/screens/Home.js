74aba62d4f4600556c0f2270d577bd0aea1f0431
davidbanu <davidbanu@ymail.com>
2021-05-31 21:29:30 +0300

    Adding quote on main page

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -21,6 +21,11 @@ function Home() {
                     <h3 className="title-presentation text-center">We are a web & app development company that turns your ideas into a new driving force of your business.</h3>
                     <p className="description-presentation text-center">We hire brilliant web and mobile app developers to deliver projects on time and maintain the top-notch code standards.</p>
                 </div>
+                <div className="quote-main text-center">
+                    <i className="fa fa-quote-left" aria-hidden="true" />
+                    <span>We help brands all over the world to create impressive products.</span>
+                    <i className="fa fa-quote-right" aria-hidden="true" />
+                </div>
             </div>
             <Footer />
         </div>
