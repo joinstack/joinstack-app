e0e8a0384b8e69452559a02d35796b70d7b19674
David Banu <davidbanu@192-168-0-163.rdsnet.ro>
2022-03-03 19:51:05 +0200

    Add new section Home page

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -21,6 +21,21 @@ function Home() {
                     <h3 className="title-presentation text-center">We are a web and mobile app development company that turns your ideas into a new driving force of your business.</h3>
                     <p className="description-presentation text-center">We work with brilliant web and mobile app developers to deliver projects on time and maintain the top-notch code standards.</p>
                 </div>
+                <div className="row numbers-presentation">
+                    <h3 className="text-center font-rubik-bold">We proudly delivered so far</h3>
+                    <div className="col-md-4 col-xs-12 padding-top-5">
+                        <p className="text-center numbers-subtitle">125</p>
+                        <p className="text-center text-numbers">Websites</p>
+                    </div>
+                    <div className="col-md-4 col-xs-12 padding-top-5">
+                        <p className="text-center numbers-subtitle">14</p>
+                        <p className="text-center text-numbers">Mobile Apps</p>
+                    </div>
+                    <div className="col-md-4 col-xs-12 padding-top-5">
+                        <p className="text-center numbers-subtitle">54</p>
+                        <p className="text-center text-numbers">Tools for developers</p>
+                    </div>
+                </div>
                 <div className="quote-main text-center">
                     <i className="fa fa-quote-left" aria-hidden="true" />
                     <span>We help brands all over the world to create impressive products.</span>
