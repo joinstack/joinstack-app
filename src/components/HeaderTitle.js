58f3560bad38e8bb8f21452301c24406f3e43790
David Banu <davidbanu@ymail.com>
2022-03-08 23:14:17 +0200

    Changing font for header

--- src/components/HeaderTitle.js
+++ src/components/HeaderTitle.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import illustration from '../assets/working-in-office.svg';
 
 function HeaderTitle() {
     var words = ["You name it, we make it", "Creating impressive products", "We bring your ideas to life", "Creating mobile and web apps for your daily needs"];  
@@ -19,9 +19,14 @@ function HeaderTitle() {
     }, 4000);
 
     return (
-        <div>
-            <h3 className="header-title">We build beautiful and scalable solutions</h3>
-            <p className="text-center sub-title height-60" id="slogan">Creating mobile and web apps for your daily needs</p>
+        <div className="row container-header">
+            <div className="col-md-6 col-xs-12">
+                <h3 className="header-title">We build beautiful and scalable solutions</h3>
+                <p className="text-center sub-title height-60" id="slogan">Creating mobile and web apps for your daily needs</p>
+            </div>
+            <div className="col-md-6 col-xs-12">
+                <img src={illustration} class="illustration-img text-center"/>
+            </div>
         </div>
     )
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,8 +3,6 @@ import React from 'react';
 import HeaderTitle from '../components/HeaderTitle';
 import Footer from '../components/Footer';
 import TopNavBar from '../components/TopNavBar';
-
-import illustration from '../assets/working-in-office.svg';
 import ProcessCards from '../components/ProcessCards';
 import QuoteHome from '../components/QuoteHome';
 import PerformanceNumbers from '../components/PerformanceNumbers';
@@ -16,13 +14,10 @@ function Home() {
     return (
         <div className="flex-wrapper">
             <div className="container container-main-page">
-                <TopNavBar/>
-                <HeaderTitle/>
-                <img src={illustration} class="illustration-img text-center"/>
-                <div className="max-width-800">
-                    <h3 className="title-presentation text-center">We are a web and mobile app development company that turns your ideas into a new driving force of your business.</h3>
-                    <p className="description-presentation text-center">We work with brilliant web and mobile app developers to deliver projects on time and maintain the top-notch code standards.</p>
-                </div>
+                <TopNavBar/>  
+                <HeaderTitle/>       
+                <h3 className="title-presentation text-center">We are a web and mobile app development company that turns your ideas into a new driving force of your business.</h3>
+                <p className="description-presentation text-center">We work with brilliant web and mobile app developers to deliver projects on time and maintain the top-notch code standards.</p>
                 <PerformanceNumbers/>
                 <ProcessCards/>
                 <QuoteHome/>
