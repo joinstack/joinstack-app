7cab6d5f7f629e3c7506cf68a440515b4363fc93
David Banu <davidbanu@ymail.com>
2022-05-11 02:59:32 +0300

    Change attribute

--- src/components/FastVideoSection.js
+++ src/components/FastVideoSection.js
@@ -6,7 +6,7 @@ function FastVideoSection() {
         <div className="row process-main">
             <div className="col-md-6 col-xs-12">
                 <div class="embed-responsive embed-responsive-16by9">
-                    <video class="embed-responsive-item" src={video} autoPlay loop muted playsinline></video>
+                    <video class="embed-responsive-item" src={video} autoplay="autoplay" loop muted playsinline></video>
                 </div>
             </div>
             <div className="col-md-6 col-xs-12">
